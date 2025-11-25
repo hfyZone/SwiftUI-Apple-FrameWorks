49c0b9014079dc0caa1fec89d1d6bfdffc8c2e49
hfyZone <1178805691@qq.com>
2025-11-25 13:38:19 +0800

    Initial Commit

---
+++ SwiftUI-Apple-FrameWorks/SwiftUI_Apple_FrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_FrameWorksApp.swift
+//  SwiftUI-Apple-FrameWorks
+//
+//  Created by 韩飞洋 on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_FrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
